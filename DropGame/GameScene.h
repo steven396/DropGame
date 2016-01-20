8d978928ef5875ec45ce477f10b1025021e309d5
XinWang <xinwang396@outlook.com>
2016-01-20 13:25:12 +0000

    add player
    add alien

--- DropGame/GameScene.h
+++ DropGame/GameScene.h
@@ -10,7 +10,7 @@
 #import "cocos2d.h"
 
 @interface GameScene : CCLayer {
-    
+    CCSprite * alien;
 }
 +(id)scene;
 @end
--- DropGame/GameScene.m
+++ DropGame/GameScene.m
@@ -18,4 +18,17 @@
 	return scene;
 }
 
+-(id)init{
+    if (self = [super init]) {
+        self.accelerometerEnabled = YES;
+        
+        CGSize screenSize = [[CCDirector sharedDirector]winSize];
+        alien = [CCSprite spriteWithFile:@"alien.png"];
+        float alienHeight = [alien texture].contentSize.height;
+        alien.position = CGPointMake(screenSize.width * 0.5, alienHeight * 0.5);
+        [self addChild:alien z:0 tag:1];
+    }
+    return self;
+}
+
 @end
